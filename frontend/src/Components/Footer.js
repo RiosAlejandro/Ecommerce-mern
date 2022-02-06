285912cbb62d10512ad60edd7ad9fb7b63e254a1
RiosAlejandro <alexandro.aa86@gmail.com>
2022-02-05 23:40:47 -0300

    avances diseño en footer y nav

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import { faFacebookSquare, faInstagramSquare, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
+import iconMate from '../Assets/mate3.png';
 import '../App.css';
 
 
 const Footer = () => {
     return ( 
-        <footer className='conteiner-fluid'>
+        <footer className='conteiner-fluid fonFooder'>
 
-            <div className='row'>
+            <div className='row pb-4'>
                 <div className='col-md-6 d-flex justify-content-around'>
                     <div className='d-flex flex-column'>
                         <h4><u>Institucional</u></h4>
@@ -34,34 +35,34 @@ const Footer = () => {
                         <a>Venta mayorista (a locales)</a>
                     </div>
                 </div>
-                <div className='col-md-6'>
+                <div className='col-md-6 d-flex justify-content-around align-items-center'>
                     <div>
                         <FontAwesomeIcon icon={faMapMarkerAlt}/>
-                        <dt>Locales</dt>
+                        <dt className='text-center'>Locales</dt>
                     </div>
                     <div>
                     <FontAwesomeIcon icon={faFacebookSquare}/>
-                        <dt>FB</dt>
+                        <dt className='text-center'>FB</dt>
                     </div>
                     <div>
                     <FontAwesomeIcon icon={faTwitterSquare}/>
-                        <dt>TW</dt>
+                        <dt className='text-center'>TW</dt>
                     </div>
                     <div>
                     <FontAwesomeIcon icon={faInstagramSquare}/>
-                        <dt>IG</dt>
+                        <dt className='text-center'>IG</dt>
                     </div>
                 </div>
             </div>
 
             <hr />
 
-            <div className='row'>
+            <div className='row py-2'>
                 <div className='col-md-6'>
-                    <p>&copy; 2021. Alejandro Rios. <u>Términos y condiciones</u></p>
+                    <p className='margCopy'>&copy; 2021. Alejandro Rios. <u>Términos y condiciones</u></p>
                 </div>
-                <div className='col-md-6 text-left'>
-                    <i></i>
+                <div className='col-md-6 alignMate'>
+                    <img src={iconMate} alt='icon mate' className='icoMat'/>
                     <span>Tome un buen mate</span>
                 </div>
             </div>
--- frontend/src/Components/Nav.js
+++ frontend/src/Components/Nav.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import '../App.css';
 
 const Nav = () => {
     return ( 
         <>
-            <div className='row'>
+            <div className='row navegacion'>
                 <div className='col-md-8'>
                     <ul className="nav nav-pills">
                         <li className="nav-item">
@@ -16,12 +19,14 @@ const Nav = () => {
                             <a className="nav-link" href="#">Link</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link disabled" href="#">Disabled</a>
+                            <a className="nav-link" href="#">Disabled</a>
                         </li>
                     </ul>
                 </div>
                 <div className='col-md-4 d-flex justify-content-around'>
+                    <FontAwesomeIcon icon={faUser} />
                     <span>hola </span>
+                    <FontAwesomeIcon icon={faShoppingCart} />
                     <span>mundo </span>
                 </div>
             </div>
